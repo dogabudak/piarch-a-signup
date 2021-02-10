210579c511337c75cfec869fb17fa16cdd2bccfd
doga <doga.budak@check24.de>
2021-02-10 11:41:59 +0100

    changed mongo lib and changed local db to mlab db

--- index.js
+++ index.js
@@ -3,60 +3,57 @@
  */
 
 
-const mongojs = require('mongojs'),
-    jwt = require('jsonwebtoken'),
-    config = require('./resources/config.js'),
-    db = mongojs(config.mongo.url, ['users']),
-    fs = require('fs'),
-    private_key = fs.readFileSync('./keys/' + 'piarch_a'),
-    koa = require('koa'),
-    route = require('koa-route');
+const jwt = require('jsonwebtoken');
+const config = require('./resources/config.js');
+const fs = require('fs');
+const private_key = fs.readFileSync('./keys/' + 'piarch_a');
+const koa = require('koa');
+const route = require('koa-route');
 
-var app = koa();
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(config.mongo.url, {useNewUrlParser: true});
 
+const app = koa();
 app.use(route.get('/signup', newSignup));
 
 app.listen(config.server.port);
 
-function * newSignup() {
+function* newSignup() {
     this.body = yield signUp(this.header);
 }
-function signUp(req) {
-    return new Promise(function (fulfill, reject) {
-        var username = req.username;
-        var password = req.password;
-        if (username.length < 3 || password.length < 3) {
-            fulfill(null)
-        }
 
-        db.users.find({"username": username}, function (err, reply) {
-            if (reply[0] === undefined) {
-                if ((err === null || err === undefined)) {
-                    var data = {
-                            sub: username,
-                            iss: 'piarch_a'
-                        },
-                        options = {
-                            algorithm: 'RS256',
-                            expiresIn: (10 * 60 * 60),
-                        };
-                    db.users.insert({"username": username, "password": password}, function (err, reply) {
-                        if (err) {
-                          reject({})
-                          //TODO handle errors
-                        } else {
-                            delete reply._id
-                            reply.token = jwt.sign(data, private_key, options);
-                            fulfill(reply)
-                        }
-                    })
-                } else {
-                    reject({})
-                }
-            }
-            else {
+function signUp(req) {
+  return new Promise(function (fulfill, reject) {
+    client.connect(err => {
+      var username = req.username;
+      var password = req.password;
+      console.log(username, password)
+      if (username.length < 3 || password.length < 3) {
+        fulfill(null)
+      }
+      const collection = client.db("piarka").collection("users");
+        collection.find({"username": username}).toArray(function (err, reply) {
+        if (reply[0] === undefined) {
+          if ((err === null || err === undefined)) {
+          var data = {sub: username, iss: 'piarch_a'}, options = {algorithm: 'RS256', expiresIn: (10 * 60 * 60)};
+            collection.insertMany([{"username": username, "password": password} ], function (err, reply) {
+              if (err) {
                 reject({})
-            }
+                //TODO handle errors
+                } else {
+                  delete reply._id
+                  reply.token = jwt.sign(data, private_key, options);
+                  fulfill(reply)
+                }
+              });
+              } else {
+                reject({})
+              }
+              } else {
+                reject({})
+              }
+            })
         })
+
     })
 }
